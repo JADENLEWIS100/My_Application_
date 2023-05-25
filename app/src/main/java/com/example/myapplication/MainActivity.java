48e9f1e48bbfc7f27a8149807c51c845b04edf59
Jaden Lewis <lewisjaden100@gmail.com>
2023-05-25 11:51:12 -0700

    updated gradle

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,13 +2,22 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnNextPage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+       btnNextPage = findViewById(R.id.btn_nextpage);
+
+       Intent secondaryActivity = new Intent(MainActivity.this, MainActivity2.class);
+
+       String sendThisString = "Whatever string I want to send"
     }
 }
