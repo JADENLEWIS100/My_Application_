e912672081c7522f58f6336f5ab7d577262f7402
Jaden Lewis <lewisjaden100@gmail.com>
2023-06-01 11:52:48 -0700

    updated everything

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -18,21 +19,34 @@ public class MainActivity extends AppCompatActivity {
     private EditText etFood;
     private EditText etCeleb;
     private Button btnNextPage;
+    private Button btnGenerate;
+
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-       btnNextPage = findViewById(R.id.btn_nextpage);
+
        etNoun = findViewById(R.id.et_noun);
        etVerb = findViewById(R.id.et_verb);
        etAdj = findViewById(R.id.et_adj);
        etFood = findViewById(R.id.et_food);
        etCeleb = findViewById(R.id.et_celeb);
+       btnNextPage = findViewById(R.id.btnNextpage);
+       btnGenerate = findViewById(R.id.btnGenerate);
 
        Intent secondaryActivity = new Intent(MainActivity.this, MainActivity2.class);
 
-       String sendThisString = "Whatever string I want to send";
+       btnGenerate.setOnClickListener({
+               @Override
+                public void onClick(){
+                   String story = "";
+                   story += etNoun.getText() + " gets ready for the day and goes out for a walk.";
+                   story += " " + etAdj.getText() + " "
+        }
+       });
+
+
     }
 }
